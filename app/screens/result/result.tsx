8cdb71b72e15e3fe4ca2df4ce55c19cc346c72ed
sourav2407 <sourav.yadav@moonshinetechnology.com>
2021-03-31 23:25:48 +0530

    Developed design for result.tsx

--- app/screens/result/result.tsx
+++ app/screens/result/result.tsx
@@ -3,16 +3,21 @@ import React from 'react';
 import {Image, StatusBar, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-export const Result = props => {
+export const Result = () => {
   // const data = props.route.params.data;
   const data = {
-    appointmentTime: 'Mar 20, 2021',
-    appointerName: 'Sourav',
-    appointeeName: 'Gourav',
+    appointmentTime: '9:30 PM, Mar 20, 2021',
+    appointerName: 'Sourav Yadav',
+    appointeeName: 'Gourav Yadav',
   };
 
+  const appointmentTime = data.appointmentTime.split(',');
+  const time = appointmentTime[0].split(' ');
+  const date = appointmentTime[1].substring(1).split(' ');
+  const year = appointmentTime[2];
+
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{flex: 1}} edges={['top']}>
       <StatusBar
         animated={true}
         translucent={true}
@@ -28,7 +33,6 @@ export const Result = props => {
           width: 100,
           alignSelf: 'center',
           zIndex: 1,
-          backgroundColor: '#FFFFFF',
         }}>
         <Image
           style={{flex: 1, borderRadius: 100}}
@@ -38,19 +42,64 @@ export const Result = props => {
       <View
         style={{
           flex: 1,
-          borderWidth: 2,
+          borderWidth: 5,
           borderTopLeftRadius: 30,
           borderTopRightRadius: 30,
           marginTop: 75,
           alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: 'red',
+          backgroundColor: '#FFFFFF',
         }}>
-        <Text style={{fontSize: 35}}>{data.appointeeName}</Text>
-        <Text style={{fontSize: 18}}>have an appointment with</Text>
-        <Text style={{fontSize: 35}}>{data.appointerName}</Text>
-        <Text style={{fontSize: 18}}>on</Text>
-        <Text style={{fontSize: 35}}>{data.appointmentTime}</Text>
+        <View
+          style={{
+            alignItems: 'center',
+            height: '100%',
+            paddingHorizontal: 40,
+          }}>
+          <Text style={{fontSize: 35, marginTop: 65, fontWeight: '800'}}>
+            {data.appointeeName}
+          </Text>
+          <Text
+            style={{
+              fontSize: 20,
+              marginVertical: 30,
+              fontWeight: '500',
+              color: '#4F5266',
+            }}>
+            have an appointment with
+          </Text>
+          <Text style={{fontSize: 35, fontWeight: '800'}}>
+            {data.appointerName}
+          </Text>
+          <Text
+            style={{
+              fontSize: 20,
+              marginTop: 25,
+              marginBottom: 30,
+              fontWeight: '500',
+              color: '#4F5266',
+            }}>
+            on
+          </Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize: 55, letterSpacing:2,fontWeight: '600', color: '#dd3333'}}>
+              {time[0]}{' '}
+            </Text>
+            <Text style={{fontSize: 55, letterSpacing:2,fontWeight: '600', color: '#3797F1'}}>
+              {time[1]}
+            </Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize: 55, letterSpacing:2,fontWeight: '600', color: '#3797F1'}}>
+              {date[0]}{' '}
+            </Text>
+            <Text style={{fontSize: 55, letterSpacing:2,fontWeight: '600', color: '#dd3333'}}>
+              {date[1]}
+            </Text>
+          </View>
+          <Text style={{fontSize: 50, letterSpacing:2,fontWeight: '600', color: '#0175E3'}}>
+            {year}
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
